269d574c489621cfd34725f184833b84a5de0df6
Tom Gustafsson <jkinnala@gmail.com>
2013-11-18 19:55:05 +0200

    bilin_asm has been vectorized

--- assembly.py
+++ assembly.py
@@ -1,5 +1,8 @@
 import numpy as np
 from meshtools import *
+from scipy.sparse import coo_matrix
+
+import ipdb
 
 dunavant1 = np.array([
     3.333333333333333333333e-01,
@@ -22,6 +25,32 @@ def triquad(d):
     n = d.size/3
     return np.vstack((d[0:n],d[n:2*n])).T, d[2*n:3*n]
 
+def affine_map_vec(mesh):
+    """Build affine mappings for all triangles."""
+
+    A = {0:{},1:{}}
+
+    A[0][0] = mesh.p[mesh.t[:,1],0]-mesh.p[mesh.t[:,0],0]
+    A[0][1] = mesh.p[mesh.t[:,2],0]-mesh.p[mesh.t[:,0],0]
+    A[1][0] = mesh.p[mesh.t[:,1],1]-mesh.p[mesh.t[:,0],1]
+    A[1][1] = mesh.p[mesh.t[:,2],1]-mesh.p[mesh.t[:,0],1]
+
+    b = {}
+
+    b[0] = mesh.p[mesh.t[:,0],0]
+    b[1] = mesh.p[mesh.t[:,0],1]
+
+    detA = A[0][0]*A[1][1]-A[0][1]*A[1][0]
+
+    invA = {0:{},1:{}}
+
+    invA[0][0] = A[1][1]/detA
+    invA[0][1] = -A[0][1]/detA
+    invA[1][0] = -A[1][0]/detA
+    invA[1][1] = A[0][0]/detA
+
+    return A,b,detA,invA
+
 def affine_map(tri, mesh):
     """Build the affine mapping for 'tri'."""
     n = mesh.p[tri]
@@ -29,6 +58,24 @@ def affine_map(tri, mesh):
     b = n[0]
 
     return A,b
+
+def lin_basis_vec(qp):
+    """Return the values and gradients of linear
+    reference triangle basis functions at local
+    quadrature points (qp)."""
+    phi = {}
+
+    phi[0] = 1.-qp[:,0]-qp[:,1]
+    phi[1] = qp[:,0]
+    phi[2] = qp[:,1]
+
+    gradphi = {}
+
+    gradphi[0] = np.tile(np.array([-1.,-1.]),(qp.shape[0],1))
+    gradphi[1] = np.tile(np.array([1.,0.]),(qp.shape[0],1))
+    gradphi[2] = np.tile(np.array([0.,1.]),(qp.shape[0],1))
+
+    return phi, gradphi
 
 def lin_basis(qp):
     """Return the values and gradients of linear
@@ -48,12 +95,51 @@ def lin_basis(qp):
 
     return phi, gradphi
 
+def bilin_asm_vec(bilin, mesh):
+    """Assembly the stiffness matrix (vectorized)."""
+    N = mesh.p.shape[0]
+    T = mesh.t.shape[0]
+    K = np.zeros((N,N))
+
+    qp,qw = triquad(dunavant2)
+    phi,gradphi = lin_basis_vec(qp)
+    A,b,detA,invA = affine_map_vec(mesh)
+
+    data = np.array([])
+    rows = np.array([])
+    cols = np.array([])
+
+    for i in [0,1,2]:
+        u = np.tile(phi[i],(T,1))
+        ux = np.outer(invA[0][0],gradphi[i][:,0])+np.outer(invA[1][0],gradphi[i][:,1])
+        uy = np.outer(invA[0][1],gradphi[i][:,0])+np.outer(invA[1][1],gradphi[i][:,1])
+        for j in range(0,i+1):
+            v = np.tile(phi[j],(T,1))
+            vx = np.outer(invA[0][0],gradphi[j][:,0])+np.outer(invA[1][0],gradphi[j][:,1])
+            vy = np.outer(invA[0][1],gradphi[j][:,0])+np.outer(invA[1][1],gradphi[j][:,1])
+            
+            #K[i,j] += np.dot(qw,bilin(u,v,ux,uy,vx,vy,0,0))*np.abs(detA)
+            #ipdb.set_trace()
+            x = np.dot(bilin(u,v,ux,uy,vx,vy,0,0),qw)*np.abs(detA)
+            data = np.append(data,x)
+            rows = np.append(rows,mesh.t[:,i])
+            cols = np.append(cols,mesh.t[:,j])
+            if i != j:
+                data = np.append(data,x)
+                rows = np.append(rows,mesh.t[:,j])
+                cols = np.append(cols,mesh.t[:,i])
+            #ipdb.set_trace()
+                #ipdb.set_trace()
+
+    #    K[np.ix_(tri,tri)] += Ke
+    return coo_matrix((data,(rows,cols)), shape=(N,N)).todense()
+
 def bilin_asm(bilin, mesh):
     """Assembly the stiffness matrix."""
     N = mesh.p.size/2
     K = np.zeros((N,N))
 
-    qp,qw = triquad(dunavant1)
+    qp,qw = triquad(dunavant2)
     phi,gradphi = lin_basis(qp)
 
     for tri in mesh.t:
@@ -72,6 +158,9 @@ def bilin_asm(bilin, mesh):
                     v = phi[j][ix]
                     ux,uy = np.dot(invA.T,gradphi[i])
                     vx,vy = np.dot(invA.T,gradphi[j])
+
+                    ipdb.set_trace()
+
                     Ke[i,j] += w*bilin(u,v,ux,uy,vx,vy,0,0)*np.abs(detA)
 
         K[np.ix_(tri,tri)] += Ke
--- main.py
+++ main.py
@@ -5,23 +5,30 @@ mesh = square_mesh(20)
 #mesh.visualize()
 
 dudv = lambda u,v,ux,uy,vx,vy,x,y: ux*vx+uy*vy
-K = bilin_asm(dudv, mesh)
+K = bilin_asm_vec(dudv, mesh)
+#Ko = bilin_asm(dudv, mesh)
 
-fv = lambda v,vx,vy,x,y: 1.*v
-f = lin_asm(fv, mesh)
+#   fv = lambda v,vx,vy,x,y: 1.*v
+#   f = lin_asm(fv, mesh)
 
-N = mesh.p.shape[0]
-A = np.arange(0,N)
-D1 = np.nonzero(mesh.p[:,0]==0)[0]
-D2 = np.nonzero(mesh.p[:,0]==1)[0]
-D3 = np.nonzero(mesh.p[:,1]==0)[0]
-D4 = np.nonzero(mesh.p[:,1]==1)[0]
-D = np.union1d(np.union1d(np.union1d(D1,D2),D3),D4)
-F = np.setdiff1d(A,D)
+#   N = mesh.p.shape[0]
+#   A = np.arange(0,N)
+#   D1 = np.nonzero(mesh.p[:,0]==0)[0]
+#   D2 = np.nonzero(mesh.p[:,0]==1)[0]
+#   D3 = np.nonzero(mesh.p[:,1]==0)[0]
+#   D4 = np.nonzero(mesh.p[:,1]==1)[0]
+#   D = np.union1d(np.union1d(np.union1d(D1,D2),D3),D4)
+#   F = np.setdiff1d(A,D)
 
-x = np.zeros(N)
-x[F] = np.linalg.solve(K[np.ix_(F,F)],f[F])
+#   x = np.zeros(N)
+#   x[F] = np.linalg.solve(K[np.ix_(F,F)],f[F])
 
-mesh.visualize_data(x)
+#   if 0.0731 < np.max(x) and 0.0732 > np.max(x):
+#       print "OK!"
+#   else:
+#       print "ERROR IN CODE!"
+#       print np.max(x)
 
-draw_all()
+#mesh.visualize_data(x)
+
+#draw_all()
